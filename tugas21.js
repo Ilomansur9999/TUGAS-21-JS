cde9831f9b8273989d0c2748bd36ea19e6a9692b
ilomansur9999 <ilofaqot@gmail.com>
2020-05-03 22:09:21 +0700

    TUGAS 21 JS
    Tugas ini dikumpulkan ke NIOMIC

---
+++ tugas21.js
@@ -0,0 +1,12 @@
+function isi(){
+	var cari = "Saya belajar masak nasi goreng";
+	console.log(cari);
+	console.log(cari.replace("masak", "makan"))
+}
+isi();
+
+
+
+// C:\Users\USER\Documents\JS\javascript>node tugas21.js
+// Saya belajar masak nasi goreng
+// Saya belajar makan nasi goreng
